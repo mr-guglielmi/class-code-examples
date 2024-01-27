3e1959ae699c45fe1c559e8ce7b256762929f506
mr-guglielmi <philip.guglielmi@tdsb.on.ca>
2024-01-27 17:22:46 -0500

    adding examples

---
+++ Program Structure/Main.java
@@ -0,0 +1,14 @@
+/*Author: Mr. Guglielmi
+Date: 01/16/2024
+Description: This is the driver class for the project. It will instantiate objects,
+and contains the main method
+*/
+public class Main{
+
+	public static void main (String[] args){
+	
+		Beginner b = new Beginner();	//instantiate a 'Beginner' object
+		b.display(); //call display method
+		b.input(); //call input method
+	}
+}
