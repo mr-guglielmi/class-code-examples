17347b650163b4a109a95300387146fe914b8c1d
mr-guglielmi <philip.guglielmi@tdsb.on.ca>
2024-02-25 16:54:32 -0500

    Adding UML example for code outline using JGrasps UML Generator

---
+++ UML/Line.java
@@ -0,0 +1,18 @@
+public class Line{
+
+	private Point p1;
+	private Point p2;
+	
+	
+	public int solveY(int x){
+		int y = 0;
+		
+		return y;
+	}
+	
+	public void display(){
+	
+	}
+		
+	
+}
---
+++ UML/Point.java
@@ -0,0 +1,16 @@
+
+public class Point{
+
+	private int x;
+	private int y;
+	
+	
+	public int getX(){
+		return x;
+	}
+	
+	public int getY(){
+		return y;
+	}
+
+}
